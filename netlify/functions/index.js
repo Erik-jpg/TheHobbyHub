8464456974fa3815ce1cc97c5e15ccfca8085904
Erik Stone <spologas@gmail.com>
2021-09-14 17:00:56 -0700

    cannot seem to get fetch to work within function, is this why they have them sepperated?

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -1,10 +1,19 @@
+const loginBtn = document.getElementById("login");
+const createAccount = document.getElementById("createAccount");
+const HRVRDAPIKey = '620c0b3f-ae5c-4b31-adcf-efbae584408d';
 
+loginBtn.addEventListener("click", async () => {
+    document.location.href = 'http://localhost:8888';
+});
 
-exports.handler = async function () {
+searchHarvard.addEventListener("click", exports.handler = async function () {
+  const harvardArt = `https://api.harvardartmuseums.org/gallery?floor=any${HRVRDAPIKey}`
+console.log(response);
+  const response = await fetch(harvardArt)
+  const data = await response.json()
+
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: 'Hello, world!'
-    })
-}
-}
+    body: JSON.stringify(data)
+  }
+}) 
