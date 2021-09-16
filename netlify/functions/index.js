8e3cf58c9a1d9100ae07c1999cb706847007b8fc
Erik Stone <spologas@gmail.com>
2021-09-15 17:05:16 -0700

    I have a calendar and timer but index.js error 404, messed with location but to no avail, will try after reset and then ask for help

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -1,10 +1,6 @@
-const loginBtn = document.getElementById("login");
-const createAccount = document.getElementById("createAccount");
+const { toolresults_v1beta3 } = require("googleapis");
+const searchHarvard = document.querySelector('searchHarvard')
 const HRVRDAPIKey = '620c0b3f-ae5c-4b31-adcf-efbae584408d';
-
-loginBtn.addEventListener("click", async () => {
-    document.location.href = 'http://localhost:8888';
-});
 
 searchHarvard.addEventListener("click", exports.handler = async function () {
   const harvardArt = `https://api.harvardartmuseums.org/gallery?floor=any${HRVRDAPIKey}`
@@ -17,3 +13,103 @@ console.log(response);
     body: JSON.stringify(data)
   }
 }) 
+
+today = new Date();
+currentMonth = today.getMonth();
+currentYear = today.getFullYear();
+selectYear = document.getElementById("year");
+selectMonth = document.getElementById("month");
+
+months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+monthAndYear = document.getElementById("monthAndYear");
+showCalendar(currentMonth, currentYear);
+console.log(response);
+
+function next() {
+    currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear;
+    currentMonth = (currentMonth + 1) % 12;
+    showCalendar(currentMonth, currentYear);
+}
+
+function previous() {
+    currentYear = (currentMonth === 0) ? currentYear - 1 : currentYear;
+    currentMonth = (currentMonth === 0) ? 11 : currentMonth - 1;
+    showCalendar(currentMonth, currentYear);
+}
+
+function jump() {
+    currentYear = parseInt(selectYear.value);
+    currentMonth = parseInt(selectMonth.value);
+    showCalendar(currentMonth, currentYear);
+}
+
+function showCalendar(month, year) {
+
+    let firstDay = (new Date(year, month)).getDay();
+
+    tbl = document.getElementById("calendar-body"); // body of the calendar
+
+    // clearing all previous cells
+    tbl.innerHTML = "";
+
+    // filing data about month and in the page via DOM.
+    monthAndYear.innerHTML = months[month] + " " + year;
+    selectYear.value = year;
+    selectMonth.value = month;
+
+    // creating all cells
+    let date = 1;
+    for (let i = 0; i < 6; i++) {
+        // creates a table row
+        let row = document.createElement("tr");
+
+        //creating individual cells, filing them up with data.
+        for (let j = 0; j < 7; j++) {
+            if (i === 0 && j < firstDay) {
+                cell = document.createElement("td");
+                cellText = document.createTextNode("");
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+            }
+            else if (date > daysInMonth(month, year)) {
+                break;
+            }
+
+            else {
+                cell = document.createElement("td");
+                cellText = document.createTextNode(date);
+                if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth()) {
+                    cell.classList.add("bg-info");
+                } // color today's date
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+                date++;
+            }
+
+
+        }
+
+        tbl.appendChild(row); // appending each row into calendar body.
+    }
+
+}
+
+
+// check how many days in a month code from https://dzone.com/articles/determining-number-days-month
+function daysInMonth(iMonth, iYear) {
+    return 32 - new Date(iYear, iMonth, 32).getDate();
+}
+console.log(showCalendar);
+
+let time = 0;
+const timer = document.getElementById('time');
+setInterval(updateTimer, 10000);
+function updateTimer() {
+  const minutes = math.floor(time * 60);
+  let hours = time * 60;
+// minutes = minutes < 10 ? '0' + minutes : minutes;
+  
+  timer.innerHTML = `${hours} : ${minutes}`
+  time++;
+}
